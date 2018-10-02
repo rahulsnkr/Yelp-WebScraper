8be8561f434698bc1459cb9164e02841a1590aef
Rahul Sankar <rahulsnkr@gmail.com>
2018-10-03 01:38:08 +0530

    More features extracted

--- scrape.py
+++ scrape.py
@@ -6,6 +6,9 @@ import pandas as pd
 # Change this to your desired url
 DEFAULT_URL = 'https://www.yelp.com/biz/ocean-ale-house-san-francisco'
 
+# Dictionary to store extra features
+feature_dict = {}
+
 # Getting page
 r  = requests.get(DEFAULT_URL)
 data = r.text
@@ -20,17 +23,20 @@ total_pages = [int(s) for s in total_pages.split() if s.isdigit()]
 limit = total_pages[-1]
 current_page = 1
 
-# Getting name of the restaurant
-name = soup.find('h1').getText().lstrip()
-
-# Getting address
-address = soup.find('address').getText().lstrip()
-
 # Getting price range
-price_range = soup.find('dd', attrs={'class' : 'nowrap price-description'}).getText().lstrip()
+price_range = soup.find('dd', attrs={'class' : 'nowrap price-description'}).getText().strip()
+feature_dict['price_range'] = price_range
 
 # Getting health score
-health_score = soup.find('dd', attrs={'class' : 'nowrap health-score-description'}).getText().lstrip()
+health_score = soup.find('dd', attrs={'class' : 'nowrap health-score-description'}).getText().strip()
+feature_dict['health_score'] = health_score
+
+# Getting extra features
+extra_list = soup.find('div', {'class' : 'short-def-list'}).findAll('dl')
+for item in extra_list:
+    feature = item.find('dt').getText().strip().lower().replace(" ", "_")
+    value = item.find('dd').getText().strip()
+    feature_dict[feature] = value
 
 # Initializing dataframe
 dataset = pd.DataFrame(columns=('restaurant_name', 'address', 'price_range', 'health_score', 'review', 'date', 'rating'))
@@ -55,7 +61,12 @@ while(current_page <= limit):
         rating = review_rating.select('div[class*="i-stars i-stars--regular"]')
         rating = float(re.findall("\d+\.\d+", rating[0]['title'])[0])
         date = re.findall("\d+\/\d+\/\d+", review_rating.find('span', attrs={'class' : 'rating-qualifier'}).getText())[0]
-        dataset.loc[k] = [name, address, price_range, health_score, review, date, rating]
+        dataset.loc[k] = [review, date, rating]
         k+=1
         
+# Inserting the extra features
+for key, val in feature_dict.items():
+    dataset[key] = val
+
+# Writing to file
 dataset.to_csv('dataset.csv')
